0ff49dcea84b51ee9133dc0351c885a4f3395ac2
YS <sunnyskylove22@gmail.com>
2024-03-14 15:11:02 +0900

    [24.03.14] 추가

--- src/main/java/com/ohgiraffers/section01/xml/Application.java
+++ src/main/java/com/ohgiraffers/section01/xml/Application.java
@@ -12,7 +12,7 @@ public class Application {
         Scanner sc = new Scanner(System.in);
 
         do {
-            System.out.println("============= 마이바티스 동적 SQL =============");
+            System.out.println("============= ## 마이바티스 동적 SQL ## =============");
             System.out.println("1. if 확인하기");
             System.out.println("2. choose(when, otherwise) 확인하기");
             System.out.println("3. foreach 확인하기");
@@ -41,7 +41,7 @@ public class Application {
 
         do{
 
-            System.out.println("========trim 서브메뉴========");
+            System.out.println("======== #trim 서브메뉴========");
             System.out.println("1. 검색 조건이 있는 메뉴코드로 조회, 단 없으면 전체 조회");
             System.out.println("2. 메뉴 혹은 카테고리코드로 검색, 단 메뉴와 카테고리 둘 다 일치하는 경우도 검색하며, 검색 조건이 없는 경우 전체 검색");
             System.out.println("3. 원하는 메뉴 정보만 수정하기");
@@ -60,6 +60,7 @@ public class Application {
 
     }
 
+    //중요!! :  //
     private static Map<String, Object> inputChangeInfo() {
 
         Scanner sc = new Scanner(System.in);
@@ -84,6 +85,7 @@ public class Application {
 
     }
 
+    //중요 !! :
     private static Map<String, Object> inputSearchCriteriaMap() {
 
         Scanner sc = new Scanner(System.in);
@@ -117,6 +119,7 @@ public class Application {
 
     }
 
+    //중요!! :
     private static SearchCriteria inputAllOrOne() {
 
         Scanner sc = new Scanner(System.in);
@@ -133,7 +136,6 @@ public class Application {
 
         return searchCriteria;
 
-
     }
 
     private static void foreachSubMenu() {
@@ -143,7 +145,7 @@ public class Application {
 
         do {
 
-            System.out.println("============foreach 서브 메뉴============");
+            System.out.println("============ #foreach 서브 메뉴============");
             System.out.println("1. 랜덤한 메뉴 5개 조회하기. 1번을 입력하면 랜덤메뉴 5개가 출력됩니다.");
             System.out.println("9. 이전 메뉴로");
             System.out.print("메뉴 번호를 입력하세요 : ");
@@ -156,14 +158,13 @@ public class Application {
 
         } while (true);
 
-
     }
 
     private static List<Integer> createRandomMenuCodeList() {
 
         Set<Integer> set = new HashSet<>();
         while (set.size() < 5) {
-            /* 현재 내 데이터베이스에서 메뉴 번호는 1 ~ 30 번까지 이다. */
+            /* 참고. 현재 내 데이터베이스에서 메뉴 번호는 1 ~ 30 번까지 이다. */
             int temp = ((int)(Math.random() * 30)) + 1; // 랜덤하면 실수로 나올 것이기 때문에 강제형변환해준다.
             set.add(temp);
         }
@@ -174,6 +175,7 @@ public class Application {
         return list;
 
     }
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     private static void chooseSubMenu() {
 
@@ -182,7 +184,7 @@ public class Application {
 
         do {
 
-            System.out.println("============choose 서브 메뉴==============");
+            System.out.println("============ #choose 서브 메뉴==============");
             System.out.println("1. 카테고리 상위 분류별 메뉴 보여주기(식사, 음료, 디저트)");
             System.out.println("9. 이전 메뉴로 돌아가기");
             System.out.print("메뉴 번호를 입력해주세요 : ");
@@ -206,14 +208,14 @@ public class Application {
         return new SearchCriteria("category", value);
 
     }
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     private static void ifSubMenu() {
 
         Scanner sc = new Scanner(System.in);
         MenuService menuService = new MenuService();
 
         do {
-            System.out.println("===========if 서브 메뉴=============");
+            System.out.println("=========== #if 서브 메뉴=============");
             System.out.println("1. 원하는 금액대에 적합한 추천 메뉴 목록 보여주기");
             System.out.println("2. 메뉴 이름 or 카테고리명으로 검색하여 메뉴 목록 보여주기");
             System.out.println("9. 이전 메뉴로");
@@ -249,6 +251,7 @@ public class Application {
         int price = sc.nextInt();
 
         return price;
+
     }
 
 }
